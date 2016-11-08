245185a9d692ef372be5f8741a6f07c0793a093d
Ferojasrom <fabioernesto@gmail.com>
2016-11-08 09:19:41 -0500

    Primera version

---
+++ RDGpio/models.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class GPIO(models.Model):
+    pin = models.IntegerField()
+    conf = models.BooleanField(default=False)
+    state = models.BooleanField()
+    
